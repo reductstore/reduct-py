2ac9ab31f04c0e3e9fe511b4e9c589699b4a037f
aschenbecherwespe <ciaran.moyne@gmail.com>
2022-04-02 18:36:03 +0200

    add docstrings

--- pkg/reduct/client.py
+++ pkg/reduct/client.py
@@ -1,3 +1,4 @@
+"""Main client code"""
 # Python implementation of Reduct Storage HTTP API
 # (c) 2022 Alexey Timin
 import json
@@ -14,17 +15,20 @@ logger = logging.getLogger(__name__)
 
 
 class QuotaType(Enum):
+    """determines if database has fixed size"""
     NONE = "NONE"
     FIFO = "FIFO"
 
 
 class BucketSettings(BaseModel):
+    """configuration for the currently connected db"""
     max_block_size: Optional[int]
     quota_type: Optional[QuotaType]
     quota_size: Optional[int]
 
 
 class Bucket:
+    """top level storage object"""
     def __init__(
         self,
         bucket_url: AnyHttpUrl,
@@ -36,27 +40,30 @@ class Bucket:
         self.settings = settings
 
     async def read(self, entry_name: str, timestamp: float) -> bytes:
+        """read an object from the db"""
         params = {"ts": timestamp}
         async with aiohttp.ClientSession() as session:
             async with session.get(
                 f"{self.bucket_url}/b/{self.bucket_name}/{entry_name}", params=params
-            ) as response:
+                                  ) as response:
                 return await response.text()
 
     async def write(self, entry_name: str, data: bytes, timestamp=time.time()):
+        """write an object to db"""
         params = {"ts": timestamp}
         async with aiohttp.ClientSession() as session:
             async with session.post(
                 f"{self.bucket_url}/b/{self.bucket_name}/{entry_name}",
                 params=params,
                 data=data,
-            ) as response:
+                                   ) as response:
                 if response.status != 200:
-                    logger.error("error response from server: %n", response.status)
+                    logger.error("error response from server: %d", response.status)
 
     async def list(
         self, entry_name: str, start: float, stop: float
     ) -> List[Tuple[float, int]]:
+        """list all objects in bucket"""
         params = {"start": start, "stop": stop}
         async with aiohttp.ClientSession() as session:
             async with session.get(
@@ -77,6 +84,7 @@ class Bucket:
     async def walk(
         self, entry_name: str, start: float, stop: float
     ) -> AsyncIterator[bytes]:
+        """step through all objects in a bucket"""
         items = await self.list(entry_name, start, stop)
         for timestamp, _ in items:
             data = await self.read(entry_name, timestamp)
@@ -84,19 +92,21 @@ class Bucket:
 
     async def remove(self):
         """not implemented in API yet?"""
-        pass
 
 
 class ServerInfo(BaseModel):
+    """server stats"""
     version: str
     bucket_count: int
 
 
 class Client:
+    """main connection to client"""
     def __init__(self, url: AnyHttpUrl):
         self.url = url
 
     async def info(self) -> Optional[ServerInfo]:
+        """get high level server info"""
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.url}/info") as response:
 
@@ -112,6 +122,7 @@ class Client:
                     return None
 
     async def get_bucket(self, name: str) -> Bucket:
+        """load a bucket to work with"""
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.url}/b/{name}") as response:
                 logger.debug("Status: %d", response.status)
@@ -124,6 +135,7 @@ class Client:
     async def create_bucket(
         self, name: str, settings: Optional[BucketSettings] = None
     ) -> Bucket:
+        """create a new bucket"""
         async with aiohttp.ClientSession() as session:
             async with session.post(f"{self.url}/b/{name}") as response:
                 logger.debug(response)
@@ -134,6 +146,7 @@ class Client:
                     return Bucket(self.url, name, settings)
 
     async def delete_bucket(self, name: str) -> bool:
+        """remove a bucket"""
         async with aiohttp.ClientSession() as session:
             async with session.delete(f"{self.url}/b/{name}") as response:
                 logger.debug(response)
@@ -143,4 +156,4 @@ class Client:
                     return False
 
     async def update_bucket(self, settings: BucketSettings) -> bool:
-        pass
+        """update bucket settings"""
