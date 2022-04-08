2729783de257a5079ab7982de414f7a801912461
Aleksey Timin <atimin@gmail.com>
2022-04-08 19:13:05 +0200

    add test for list

--- tests/client_test.py
+++ tests/client_test.py
@@ -1,11 +1,18 @@
 """Just stub for pipline"""
+import random
+
 import pytest
-from reduct import Client, ServerInfo
+from reduct import Client, ServerInfo, BucketList
 
 
 @pytest.fixture(name="url")
 def _url() -> str:
     return "http://127.0.0.1:8383"
+
+
+@pytest.fixture(name="bucket_name")
+def _gen_bucket_name() -> str:
+    return f"bucket_{random.randint(0, 1000000)}"
 
 
 @pytest.mark.asyncio
@@ -15,3 +22,19 @@ async def test__info(url):
     info: ServerInfo = await client.info()
 
     assert info.version >= "0.4.0"
+
+
+@pytest.mark.asyncio
+async def test__list(url, bucket_name):
+    """Should browse buckets"""
+    client = Client(url)
+    bucket_1 = bucket_name + "1"
+    bucket_2 = bucket_name + "2"
+    await client.create_bucket(bucket_1)
+    await client.create_bucket(bucket_2)
+
+    ret: BucketList = await client.list()
+
+    assert len(ret.buckets) >= 2
+    assert list(filter(lambda b: b.name == bucket_1, ret.buckets))[0].size == 0
+    assert list(filter(lambda b: b.name == bucket_2, ret.buckets))[0].size == 0
