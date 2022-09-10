7dad86e70d5da960d31fa0f2241f0072ef57cd7c
ciarán moyne <moyne@panda.technology>
2022-09-10 10:00:53 +0200

    remove token renewal

--- pkg/reduct/http.py
+++ pkg/reduct/http.py
@@ -42,19 +42,6 @@ class HttpClient:
                         yield response
                         break
 
-                    if response.status == 401:
-                        # Authentication issue, try to refresh token and repeat request
-                        async with session.post(
-                            f"{self.url}/auth/refresh",
-                            headers={"Authorization": f"Bearer {self.api_token}"},
-                        ) as auth_resp:
-                            if auth_resp.status == 200:
-                                data = json.loads(await auth_resp.read())
-                                self.headers = {
-                                    "Authorization": f'Bearer {data["access_token"]}'
-                                }
-                                continue
-
                     raise ReductError(response.status, await response.text())
 
     async def request_all(self, method: str, path: str = "", **kwargs) -> bytes:
