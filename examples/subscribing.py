3dc2c67aeb4d23be1d56a03e0871c715d8b5b406
Alexey Timin <atimin@gmail.com>
2023-05-11 20:16:26 +0200

    fix subscribing

--- examples/subscribing.py
+++ examples/subscribing.py
@@ -11,11 +11,11 @@ async def writer():
     """Write a blob with toggling good flag"""
     bucket: Bucket = await client.create_bucket("bucket", exist_ok=True)
     good = True
-    while running:
+    for _ in range(21):
         data = b"Some blob of data"
         ts = int(time_ns() / 10000)
         await bucket.write("entry-1", data, ts, labels=dict(good=good))
-        print(f"Record written: ts={ts}, good={good}")
+        print(f"Writer: Record written: ts={ts}, good={good}")
         good = not good
         await asyncio.sleep(1)
 
@@ -32,19 +32,21 @@ async def subscriber():
         poll_interval=0.2,
         include=dict(good=True),
     ):
-        print(f"Good record received: ts={record.timestamp}, labels={record.labels}")
+        print(
+            f"Subscriber: Good record received: ts={record.timestamp}, labels={record.labels}"
+        )
         counter += 1
-        if counter >= 10:
-            running = False
+        if counter == 10:
             break
 
 
 # --8<-- [end:subscriber]
 
+
+async def main():
+    await asyncio.gather(writer(), subscriber())
+
+
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
-
-    loop.create_task(writer())
-    loop.create_task(subscriber())
-    while running:
-        loop.run_until_complete(asyncio.sleep(1))
+    loop.run_until_complete(main())
